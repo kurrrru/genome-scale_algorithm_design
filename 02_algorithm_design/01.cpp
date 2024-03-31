0008e045808e6878d43b77a9ebcfaec975ecc1cc
kurrrru <kurrrru8850@gmail.com>
2024-04-01 00:48:59 +0900

    [Update] Exercise 2.1

--- 02_algorithm_design/01.cpp
+++ 02_algorithm_design/01.cpp
@@ -6,50 +6,27 @@ using namespace std;
 
 int	MP(string T, string p, vector<int> &fail)
 {
-	int i = 0;
-	int j = 0;
-	while (i < T.size())
+	for (int i = 0, j = 0; i < T.size(); i++, j++)
 	{
-		if (T[i] == p[j])
-		{
-			i++;
-			j++;
-			if (j == p.size())
-				return (i - j);
-		}
-		else
-		{
-			if (j != 0)
-				j = fail[j];
-			else
-				i++;
-		}
+		if (T[i] == p[j] && j == p.size() - 1)
+			return (i - j);
+		while (j > 0 && T[i] != p[j])
+			j = fail[j - 1];
 	}
 	return (-1);
 }
 
-void	calc_fail(string T, vector<int> &fail)
+void	calc_fail(string p, vector<int> &fail)
 {
-	int i = 1;
-	int j = 0;
-	fail[0] = 1;
-	while (i < T.size())
+	for (int i = 0; i < p.size(); i++)
+		fail[i] = 0;
+	for (int i = 1, j = 0; i < p.size(); i++)
 	{
-		if (T[i] == T[j])
-		{
-			fail[i] = j + 1;
-			i++;
-			j++;
-		}
-		else
-		{
-			if (j != 0)
-				j = fail[j];
-			else
-				i++;
-		}
+		while (j > 0 && p[i] != p[j])
+			j = fail[j - 1];
+		if (p[i] == p[j])
+			fail[i] = ++j;
 	}
-	
 }
 
 int main()
@@ -57,10 +34,11 @@ int main()
 	string T; //  = "ATADDAAATA";
 	string p; // = "D";
 	cin >> T >> p;
-	vector<int> fail(T.size(), 0);
+	vector<int> fail(p.size(), 0);
 	// int fail[10] = {1, 0, 0, 0, 0, 1, 1, 1, 2, 0};
-	calc_fail(T, fail);
-	for (int i = 0; i < fail.size(); i++)
-		cout << fail[i] << " ";
+	calc_fail(p, fail);
+	// for (int i = 0; i < p.size(); i++)
+	// 	cout << fail[i] << " ";
+	// cout << endl;
 	cout << MP(T, p, fail) << endl;
 }
